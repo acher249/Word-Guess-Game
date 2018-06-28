f03184f4d185ba5c67c8b26cf18bbdbef950dc1f
Acher249 <jchernick2010@gmail.com>
2018-06-28 18:38:48 -0400

    Added Functionality

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -18,8 +18,8 @@ console.log(LettersInWord);
 
 //Link to HTML tags
 
-var userText = document.getElementById("user-text");
-var computerText = document.getElementById("computer-text");
+var letterChoiceText = document.getElementById("letterChoice-text");
+var guessesRemainingText = document.getElementById("guessesRemaining-text");
 var winsText = document.getElementById("wins-text");
 var losesText = document.getElementById("loses-text");
 var tiesText = document.getElementById("ties-text");
@@ -29,28 +29,33 @@ var wins = 0;
 var loses = 0;
 var ties = 0;
 
+var guessNumber = 0;
+var guessRemaining = 0;
+
 document.onkeyup = function(event) {
 
-    //do all the things:
+    if(guessNumber<15){
 
-    userText.textContent = event.key;
-    var userLetterChoice = event.key;
-    console.log(userLetterChoice);
-
-    //Loop through the letters in chosenWord, if any letters match to the user choice, do somthing.
-    for (i = 0; i < LettersInWord.length; i++){
-        if(userLetterChoice == LettersInWord[i]) {
-            console.log(LettersInWord[i] + " matches a letter!");
-            //Do somthing.
+        letterChoiceText.textContent = event.key;
+        var userLetterChoice = event.key;
+        console.log(userLetterChoice);
+        guessNumber++;
+    
+        //Loop through the letters in chosenWord, if any letters match to the user choice, do somthing.
+        for (i = 0; i < LettersInWord.length; i++){
+            if(userLetterChoice == LettersInWord[i]) {
+                console.log(LettersInWord[i] + " matches a letter!");
+                //Do somthing.
+            }
+            
         }
-        
+    
+        guessRemaining = 15 - guessNumber;
+    
+        // tie back to html text.
+        guessesRemainingText.textContent = guessRemaining;
+        // losesText.textContent = loses;
+        // winsText.textContent = wins;
     }
-
-
-    // tie back to html text.
-    // computerText.textContent = computerChoice;
-    // tiesText.textContent = ties;
-    // losesText.textContent = loses;
-    // winsText.textContent = wins;
 
 };
