ed7d159997fa2d69c3eccc0c35e049b266376225
Acher249 <jchernick2010@gmail.com>
2018-06-28 21:32:34 -0400

    JS work

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -2,15 +2,25 @@
 //****************************/
 
 //need to make this word.to lowercase...
-var chosenWord = "apple";
+var chosenWord = "appleappleapple";
+var chosenWordUnderlines = [];
 var wordLength = chosenWord.length;
 
 var LettersInWord = [];
 
+//this is taking word length and making string of _,_,_
 for (i = 0; i < chosenWord.length; i++){
     var currentLetter = chosenWord[i];
     LettersInWord.push(currentLetter);
+    chosenWordUnderlines.push("_");
 }
+
+//Create array of underlines
+chosenWordUnderlines = chosenWordUnderlines.join();
+
+chosenWordUnderlinesString = chosenWordUnderlines.toString();
+console.log(chosenWordUnderlinesString);
+
 
 console.log(LettersInWord);
 
@@ -20,9 +30,13 @@ console.log(LettersInWord);
 
 var letterChoiceText = document.getElementById("letterChoice-text");
 var guessesRemainingText = document.getElementById("guessesRemaining-text");
+var currentWordText = document.getElementById("currentWord-text");
 var winsText = document.getElementById("wins-text");
 var losesText = document.getElementById("loses-text");
 var tiesText = document.getElementById("ties-text");
+
+//Send the underlined version of chosen word to the HTML tag.
+currentWordText.textContent = chosenWordUnderlinesString;
 
 //use this later after all guesses are finished.
 var wins = 0;
@@ -30,11 +44,17 @@ var loses = 0;
 var ties = 0;
 
 var guessNumber = 0;
+var allowableGuesses = 15;
 var guessRemaining = 0;
 
 document.onkeyup = function(event) {
 
-    if(guessNumber<15){
+    if(guessNumber < allowableGuesses){
+        //create array with underlines.. in order to sub out letters
+        var chosenWordUnderlines = [];
+        for (i = 0; i < chosenWord.length; i++){
+            chosenWordUnderlines.push("_");
+        }
 
         letterChoiceText.textContent = event.key;
         var userLetterChoice = event.key;
@@ -45,12 +65,20 @@ document.onkeyup = function(event) {
         for (i = 0; i < LettersInWord.length; i++){
             if(userLetterChoice == LettersInWord[i]) {
                 console.log(LettersInWord[i] + " matches a letter!");
-                //Do somthing.
+
+                var currentLetter = LettersInWord[i];
+                chosenWordUnderlines.push(currentLetter);
             }
-            
         }
+        
+        //Create array of underlines
+        chosenWordUnderlines = chosenWordUnderlines.join();
+        
+        chosenWordUnderlinesString = chosenWordUnderlines.toString();
     
-        guessRemaining = 15 - guessNumber;
+        console.log("String Output" + chosenWordUnderlinesString);
+
+        guessRemaining = allowableGuesses - guessNumber;
     
         // tie back to html text.
         guessesRemainingText.textContent = guessRemaining;
